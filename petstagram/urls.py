01f93bedf9c348558769165fb2589c43e8b3ec7d
Vasil <vrangelov85@abv.bg>
2022-09-30 14:28:12 +0300

    Initial commit

---
+++ petstagram/urls.py
@@ -0,0 +1,9 @@
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/4.1/topics/http/urls/
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
