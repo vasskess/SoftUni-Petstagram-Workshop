d7b04ce5e752bf8878f43ae6b2914b6149e418be
Vasil <vrangelov85@abv.bg>
2022-10-12 16:38:30 +0300

    made custom validater for uploaded image

--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -2,10 +2,11 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 from petstagram.pets.models import Pet
+from petstagram.photos.validators import validate_file_size
 
 
 class Photo(models.Model):
-    photo = models.ImageField()
+    photo = models.ImageField(validators=(validate_file_size,))
     description = models.TextField(max_length=300, validators=(MinValueValidator(10),), blank=True, null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
     tagged_pets = models.ManyToManyField(Pet, blank=True)
