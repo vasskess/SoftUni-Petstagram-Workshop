6e679db3b1505ef2bcecd30b913f0f78e3addf96
Vasil <vrangelov85@abv.bg>
2022-10-12 16:12:03 +0300

    added Pet model and DB config

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(max_length=30)
+    personal_photo = models.URLField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    slug = models.SlugField(unique=True)
