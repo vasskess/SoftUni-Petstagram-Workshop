cf8780553ce72f5b57c8f5a95fb90ebed17f6979
Vasil <vrangelov85@abv.bg>
2022-10-13 20:28:48 +0300

    added some more functionality

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -4,4 +4,5 @@ from petstagram.common.views import *
 urlpatterns = (
     path("", home_page, name="home-page"),
     path("like/<int:photo_id>/", like_functionality, name="like"),
+    path("share/<int:photo_id>/", copy_link_to_clipboard, name="share"),
 )
